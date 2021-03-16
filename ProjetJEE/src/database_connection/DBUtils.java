9ab115f1dd247f1e4cb73799755398f135e9e927
AAsmaou <aamadour@enssat.fr>
2021-03-16 21:54:39 +0100

    update files

---
+++ ProjetJEE/src/database_connection/DBUtils.java
@@ -0,0 +1,18 @@
+package database_connection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import models.User;
+import models.Album;
+import models.Playlist;
+import models.Song;
+ 
+public class DBUtils {
+ 
+   
+}
