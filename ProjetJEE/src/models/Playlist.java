1b9cf7c5447e807e31600283acb000aad94b97a5
Francesco <gazzola95francy@hotmail.it>
2021-03-17 10:45:06 +0100

    database

--- ProjetJEE/src/models/Playlist.java
+++ ProjetJEE/src/models/Playlist.java
@@ -3,12 +3,10 @@ package models;
 public class Playlist {
 
 	private String title;
-	private int id;
 
-	public Playlist(String title, int id) {
+	public Playlist(String title) {
 		super();
 		this.title = title;
-		this.id = id;
 	}
 
 	public String getTitle() {
@@ -17,14 +15,6 @@ public class Playlist {
 
 	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 	
 }
